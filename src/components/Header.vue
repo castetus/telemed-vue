46e7b1a0278846eca1dbe9ca7f128d79aa634988
castetus <kboikov@mail.ru>
2021-03-24 11:02:24 +0300

    feat: add components

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  header 
+    .container 
+      .header 
+        logo
+        h2.heading_green.header__slogan Консультируйтесь с 
+          br 
+          | профессиональными врачами 24/7 
+          br 
+          | без ожидания и очередей!
+        .header__cta 
+          a.btn.btn_outlined.btn_centered.btn_popup(data-target="#popup-form") заказать полис
+        contacts
+        button.header__menu-button#dropdown
+</template>
+
+<script>
+import Logo from './Logo'
+import Contacts from './Contacts'
+export default {
+  name: 'Header',
+  components: {
+    Logo,
+    Contacts
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .logo 
+    img.logo__img(src="img/logo.jpg")
+    span.logo__title телемед полис
+    span.logo__text ключ к вашему здоровью
+</template>
+
+<script>
+export default {
+  name: 'Logo'
+}
+</script>
+
+<style>
+
+</style>
